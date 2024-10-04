c5608d1792d3caeec47d89a95122468da2d731fc
amine-zzr <aminezoukri@gmail.com>
2024-10-04 01:20:39 +0100

    fixing the max line limit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,7 +15,8 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         prev_row = triangle[-1]
-        row = [1] + [prev_row[j] + prev_row[j + 1] for j in range(len(prev_row) - 1)] + [1]
+        row = [1] + [prev_row[j] + prev_row[j + 1] for j in range(
+              len(prev_row) - 1)] + [1]
         triangle.append(row)
 
     return triangle
