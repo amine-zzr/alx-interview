a3667ed04e6dde55e7adb41918a04178aba2fced
amine-zzr <aminezoukri@gmail.com>
2024-11-15 20:40:12 +0100

    fixing code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,34 +5,23 @@ UTF-8 Validation
 
 
 def validUTF8(data):
-    # Number of bytes left to check in the current character
-    num_bytes = 0
 
-    # Masks to identify the leading byte pattern
-    mask1 = 1 << 7
-    mask2 = 1 << 6
+    count = 0
 
     for byte in data:
-        # Only keep the last 8 bits of the byte
-        byte = byte & 0xFF
+        byte |= 256
 
-        if num_bytes == 0:
-            # Determine the number of bytes in this UTF-8 character
-            if (byte & mask1) == 0:
-                # 1-byte character
-                continue
-            elif (byte & (mask1 >> 1)) == (mask1 >> 1):
-                num_bytes = 1
-            elif (byte & (mask1 >> 2)) == (mask1 >> 2):
-                num_bytes = 2
-            elif (byte & (mask1 >> 3)) == (mask1 >> 3):
-                num_bytes = 3
-            else:
-                return False
-        else:
-            # Check that each byte starts with "10"
-            if not (byte & mask1 and not (byte & mask2)):
-                return False
-            num_bytes -= 1
+        if (byte >> 3 == 0b1_11111 or
+                (byte >> 6 == 0b1_10) ^ (count > 0)):
+            return False
 
-    return num_bytes == 0
+        if byte >> 5 == 0b1_110:
+            count = 1
+        elif byte >> 4 == 0b1_1110:
+            count = 2
+        elif byte >> 4 == 0b1_1111:
+            count = 3
+        elif byte >> 6 == 0b1_10:
+            count -= 1
+
+    return not count
